fcaa2156757bc574d04f035e21bfd0135be0ea18
hyz218 <dlsal1890@naver.com>
2021-09-18 23:11:08 +0900

    Add BFS example

--- dfs_bfs.py
+++ dfs_bfs.py
@@ -113,3 +113,39 @@ def freeze_beverage():
     return result
 
 
+#BFS
+#미로탈출
+
+from collections import deque #큐를 사용하기 위한 라이브러리 load
+
+def maze():
+    n,m = map(int,input().split())
+    
+    graph = []
+    for i in range(n):
+        graph.append(list(map(int,input())))
+        
+    dx = [-1,1,0,0] #상하좌우 방향
+    dy = [0,0,-1,1]
+    
+    def bfs(x,y):
+        queue = deque() #큐 구현을 위한 deque 라이브러리 사용
+        queue.append((x,y)) 
+        
+        while queue: #큐가 빌 때 까지 반복
+            x,y = queue.popleft()
+            
+            for i in range(4): #네 방향으로 위치 확인
+                nx = x+dx[i]
+                ny = y+dy[i]
+                
+                if nx<0 or ny<0 or nx>=n or ny>=m: #공간을 벗어난 경우 무시
+                    continue
+                if graph[nx][ny]==0: #벽인 경우 무시
+                    continue
+                if graph[nx][ny]==1: #해당 노드를 처음 방문하는 경우 최단거리 기록
+                    graph[nx][ny]=graph[x][y] + 1 #방문 표시
+                    queue.append((nx,ny)) #추가하기
+        return graph[n-1][m-1] #가장 오른쪽 아래까지의 최단거리 반환  
+    
+    return bfs(0,0)#현재 위치에서 BFS 수행
