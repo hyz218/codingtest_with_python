68429eac1fe9a83a360fe42a21bc6a414e11f717
hyz218 <dlsal1890@naver.com>
2021-10-18 01:33:18 +0900

    Practice Codingtest - Implement problems

--- Problems/Implement_problems.py
+++ Problems/Implement_problems.py
@@ -188,3 +188,65 @@ def build_frame():
     return solution(5,[[1,0,0,1],[1,1,1,1],[2,1,0,1],[2,2,1,1],[5,0,0,1],[5,1,0,1],[4,2,1,1],[3,2,1,1]])
 
 #print(build_frame)
+
+from itertools import combinations
+
+def delivery_chicken():
+    n,m = map(int, input().split())
+    chicken,house = [],[]
+    
+    for r in range(n):
+        data=list(map(int, input().split()))
+        for c in range(n):
+            if data[c]==1:
+                house.append((r,c)) #일반 집
+            elif data[c]==2:
+                chicken.append((r,c)) #치킨집
+
+    candidates = list(combinations(chicken,m)) #치킨집 m개 조합
+    
+    def get_sum(candidate): #치킨 거리의 합 계산 함수
+        result = 0 #모든 집에 대하여
+        for hx,hy in house: #가장 가까운 치킨집 찾기
+            temp = 1e9
+            for cx, cy in candidate:
+                temp = min(temp,abs(hx-cx)+abs(hy-cy)) #가장 가까운 치킨집까지 거리 더하기
+            result+=temp 
+        return result #치킨 거리의 합 반환
+
+    result = 1e9 #치킨 거리의 합 최소를 찾아 출력
+    for candidate in candidates:
+        result = min(result,get_sum(candidate))
+
+    return result
+
+#print(delivery_chicken())
+
+from itertools import permutations
+def exteriorwall_inspection():
+
+    def solution(n, weak, dist): 
+        answer = 0
+        length = len(weak) #길이를 2배로 늘려 원형을 일자 형태로 변형
+        for i in range(length):
+            weak.append(weak[i]+n)
+        answer = len(dist)+1 #투입할 친구 수의 최솟값을 찾아야하므로 len(dist)+1로 초기화
+        for start in range(length): #0부터 length-1으로 위치를 각각 시작점으로 설정
+            for friends in list(permutations(dist,len(dist))):
+                count=1 #투입할 친구의 수
+                position = weak[start] + friends[count-1] #해당 친구가 점검할 수 있는 마지막 위치
+                for index in range(start, start+length): #시작점부터 모든 취약 지점 확인
+                    if position<weak[index]: #점검할 수 있는 위치를 벗어난 경우
+                        count+=1 #새로운 친구 투입
+                        if count>len(dist): #더 투입이 불가하면 종료
+                            break
+                        position = weak[index] + friends[count-1]
+                answer = min(answer,count) #최솟값 계산
+        if answer>len(dist):
+            return -1
+            
+        return answer
+    
+    return solution(12, [1,5,6,10], [1,2,3,4])
+
+#print(exteriorwall_inspection())
